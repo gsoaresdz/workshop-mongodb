58aa949c8ca07a4c94dc76e9cc5ec97a428a3eab
Gabriel Soares <gsoaresdesouza@live.com>
2024-12-18 17:51:50 -0300

    Author, Comment and Post

--- src/main/java/com/devsuperior/workshopmongo/models/entities/User.java
+++ src/main/java/com/devsuperior/workshopmongo/models/entities/User.java
@@ -1,8 +1,11 @@
 package com.devsuperior.workshopmongo.models.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "users")
@@ -13,6 +16,9 @@ public class User implements Serializable {
 	private String id;
 	private String name;
 	private String email;
+
+	@DBRef(lazy = true)
+	public List<Post> posts = new ArrayList<>();
 
 	public User() {
 	}
