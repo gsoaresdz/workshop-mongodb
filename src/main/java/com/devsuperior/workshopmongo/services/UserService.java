38851e63a9978739f47515e024f87fa3130cd46a
Gabriel Soares <gsoaresdesouza@live.com>
2025-01-21 08:39:52 -0300

    PUT User

--- src/main/java/com/devsuperior/workshopmongo/services/UserService.java
+++ src/main/java/com/devsuperior/workshopmongo/services/UserService.java
@@ -22,13 +22,12 @@ public class UserService {
 		List<User> list = repository.findAll();
 		return list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 	}
-	
+
 	public UserDTO findById(String id) {
-		Optional<User> result = repository.findById(id);
-		User entity = result.orElseThrow(() -> new ResourceNotFoundException("Objeto não encontrado"));
+		User entity = getEntityById(id);
 		return new UserDTO(entity);
 	}
-	
+
 	public UserDTO insert(UserDTO dto) {
 		User entity = new User();
 		copyDtoToEntity(dto, entity);
@@ -36,8 +35,20 @@ public class UserService {
 		return new UserDTO(entity);
 	}
 
+	public UserDTO update(String id, UserDTO dto) {
+		User entity = getEntityById(id);
+		copyDtoToEntity(dto, entity);
+		entity = repository.save(entity);
+		return new UserDTO(entity);
+	}
+
 	private void copyDtoToEntity(UserDTO dto, User entity) {
 		entity.setName(dto.getName());
 		entity.setEmail(dto.getEmail());
+	}
+
+	private User getEntityById(String id) {
+		Optional<User> result = repository.findById(id);
+		return result.orElseThrow(() -> new ResourceNotFoundException("Objeto não encontrado"));
 	}
 }
