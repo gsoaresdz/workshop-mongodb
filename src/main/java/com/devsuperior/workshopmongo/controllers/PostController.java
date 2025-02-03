bbcb8e251a729c7b840723134322278a71d030b6
Gabriel Soares <gsoaresdesouza@live.com>
2025-02-03 08:16:49 -0300

    GET Post full search

--- src/main/java/com/devsuperior/workshopmongo/controllers/PostController.java
+++ src/main/java/com/devsuperior/workshopmongo/controllers/PostController.java
@@ -32,4 +32,12 @@ public class PostController {
 		return ResponseEntity.ok().body(list);
 	}
 
+	@GetMapping(value = "/fullsearch")
+	public ResponseEntity<List<PostDTO>> fullSearch(@RequestParam(value = "text", defaultValue = "") String text,
+			@RequestParam(value = "start", defaultValue = "") String start,
+			@RequestParam(value = "end", defaultValue = "") String end) {
+		List<PostDTO> list = service.fullSearch(text, start, end);
+		return ResponseEntity.ok().body(list);
+	}
+
 }
